6d90ee983a9d1f2453fa56c91dc44412f1427495
Ishini-0313 <misnfernando1@gmail.com>
2025-12-11 22:28:17 +0530

    search get mapping created

--- src/main/java/com/worksure/worksure/controller/WorkerController.java
+++ src/main/java/com/worksure/worksure/controller/WorkerController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.worksure.worksure.dto.CertificateRequest;
 import com.worksure.worksure.dto.JobExperienceRequest;
@@ -136,6 +129,11 @@ public class WorkerController {
         } else {
             return ResponseEntity.ok(worker);
         }
+    }
+
+    @GetMapping("/search")
+    public List<Worker> search(@RequestParam String keyword){
+        return workerService.search(keyword);
     }
 
 }
