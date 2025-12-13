88d35b46ec8278680a271e4fcde455e2ed19a8eb
Ishini-0313 <misnfernando1@gmail.com>
2025-12-13 12:04:27 +0530

    searchByLocAndSkill method created

--- src/main/java/com/worksure/worksure/service/WorkerService.java
+++ src/main/java/com/worksure/worksure/service/WorkerService.java
@@ -2,6 +2,7 @@ package com.worksure.worksure.service;
 
 import java.util.List;
 
+import com.worksure.worksure.dto.JobRole;
 import org.springframework.stereotype.Service;
 
 import com.worksure.worksure.entity.Worker;
@@ -20,5 +21,9 @@ public interface WorkerService {
 
     List<Worker> search(String keywords);
 
-    List<Worker> searchByLocation(String keyword);
+    //List<Worker> searchByLocation(String keyword);
+
+    //List<Worker> searchBySkill(JobRole keyword);
+
+    List<Worker> searchByLocAndSkill(String location, JobRole skill);
 }
