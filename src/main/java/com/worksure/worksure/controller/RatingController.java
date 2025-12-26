e30e3b01f2250fc31aa54d71a4db5de1cfb10f16
Mayurapabasara <mayurapabasara143@gmail.com>
2025-12-26 23:01:32 +0530

    feat: update RatingController with additional functionality

--- src/main/java/com/worksure/worksure/controller/RatingController.java
+++ src/main/java/com/worksure/worksure/controller/RatingController.java
@@ -1,4 +1,34 @@
 package com.worksure.worksure.controller;
 
+
+import com.worksure.worksure.entity.Rating;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rating")
+@CrossOrigin(origins = "http://localhost:5173")
 public class RatingController {
+
+    private final RatingRepository ratingRepository;
+
+    public RatingController(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    @PostMapping
+    public Rating addRating(@RequestBody Rating rating) {
+        return ratingRepository.save(rating);
+    }
+
+    @GetMapping("/{itemId}")
+    public List<Rating> getRatings(@PathVariable("itemId") Long itemId) {
+        return ratingRepository.findByItemId(itemId);
+    }
+
+    @GetMapping
+    public List<Rating> getAllRatings() {
+        return ratingRepository.findAll();
+    }
 }
