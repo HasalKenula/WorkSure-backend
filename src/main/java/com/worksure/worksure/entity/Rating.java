910b22f634a6a3b2c9b04260a6ffadfa7143816a
Mayurapabasara <mayurapabasara143@gmail.com>
2025-12-30 08:56:08 +0530

    Update rating entity

--- src/main/java/com/worksure/worksure/entity/Rating.java
+++ src/main/java/com/worksure/worksure/entity/Rating.java
@@ -1,4 +1,24 @@
 package com.worksure.worksure.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int stars; // 1-5 stars
+
+    // Many ratings can belong to one worker
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "worker_id") // foreign key in DB
+    private Worker worker;
 }
