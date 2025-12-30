60d5b510cfd79a1f4709640d8330ccbb463ca439
Mayurapabasara <mayurapabasara143@gmail.com>
2025-12-30 08:58:02 +0530

    Update rating repository

--- src/main/java/com/worksure/worksure/repository/RatingRepository.java
+++ src/main/java/com/worksure/worksure/repository/RatingRepository.java
@@ -1,4 +1,11 @@
 package com.worksure.worksure.repository;
 
-public interface RatingRepository {
+import com.worksure.worksure.entity.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    List<Rating> findByWorker_Id(String workerId);
 }
