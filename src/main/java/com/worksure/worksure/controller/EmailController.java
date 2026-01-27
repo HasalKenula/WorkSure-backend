050610eb531b30a03ab249e0f2c500415e28f72c
Ishini-0313 <misnfernando1@gmail.com>
2026-01-27 22:13:31 +0530

    email controller initiated

---
+++ src/main/java/com/worksure/worksure/controller/EmailController.java
@@ -0,0 +1,12 @@
+package com.worksure.worksure.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+@CrossOrigin("*")
+public class EmailController {
+
+}
