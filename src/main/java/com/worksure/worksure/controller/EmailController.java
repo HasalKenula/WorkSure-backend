f9f549201957cec44225597b02f89fef1cd5e3a7
Ishini-0313 <misnfernando1@gmail.com>
2026-01-27 22:31:35 +0530

    EmailController file created

--- src/main/java/com/worksure/worksure/controller/EmailController.java
+++ src/main/java/com/worksure/worksure/controller/EmailController.java
@@ -1,12 +1,23 @@
 package com.worksure.worksure.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.worksure.worksure.dto.EmailRequest;
+import com.worksure.worksure.service.EmailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @CrossOrigin("*")
 public class EmailController {
+    private final EmailService emailService;
 
+    public EmailController(EmailService emailService){
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+        emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+        return ResponseEntity.ok("Email sent successfully");
+    }
 }
