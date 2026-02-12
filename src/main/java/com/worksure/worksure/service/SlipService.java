31e4f7466fb2dfd83c4919dd4ba071a80ec981a2
HasalKenula <hasalkenula255@gmail.com>
2026-02-12 23:14:55 +0530

    added create slip to slipService

--- src/main/java/com/worksure/worksure/service/SlipService.java
+++ src/main/java/com/worksure/worksure/service/SlipService.java
@@ -2,9 +2,11 @@ package com.worksure.worksure.service;
 
 import org.springframework.stereotype.Service;
 
+import com.worksure.worksure.entity.Slip;
+
 
 
 @Service
 public interface SlipService {
-   
+    Slip createSlip(Slip slip);
 }
