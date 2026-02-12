1831a48843207e3249a9e9b8d1db375833efa1da
HasalKenula <hasalkenula255@gmail.com>
2026-02-12 21:41:53 +0530

    created slip Entity

---
+++ src/main/java/com/worksure/worksure/entity/Slip.java
@@ -0,0 +1,41 @@
+package com.worksure.worksure.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Slip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   
+    private Double amount;
+    private String bankName;
+    private String accountNumber;
+    private LocalDate paymentDate;
+    private String sipImageUrl;
+    private String remarks;
+    private LocalDateTime submittedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "worker_id")
+    private Worker worker;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
