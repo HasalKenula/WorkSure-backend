2e6c66b25e396b207d9c824740b5ff88d81b695a
HasalKenula <hasalkenula255@gmail.com>
2026-02-13 00:41:23 +0530

    modified service layer according to slipByworkerId

--- src/main/java/com/worksure/worksure/service/SlipService.java
+++ src/main/java/com/worksure/worksure/service/SlipService.java
@@ -1,11 +1,14 @@
 package com.worksure.worksure.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.worksure.worksure.entity.Slip;
 
-
 @Service
 public interface SlipService {
-    Slip createSlip(Slip slip);  
+    Slip createSlip(Slip slip);
+
+    List<Slip> getSlipByWorkerId(String workerId);
 }
--- src/main/java/com/worksure/worksure/service/SlipServiceImpl.java
+++ src/main/java/com/worksure/worksure/service/SlipServiceImpl.java
@@ -1,5 +1,7 @@
 package com.worksure.worksure.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,10 @@ public class SlipServiceImpl implements SlipService {
     @Override
     public Slip createSlip(Slip slip) {
         return slipRepository.save(slip);
+    }
+
+    @Override
+    public List<Slip> getSlipByWorkerId(String workerId) {
+        return slipRepository.findByWorkerId(workerId);
     }
 }
