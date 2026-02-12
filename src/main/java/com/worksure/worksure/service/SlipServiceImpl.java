02dbfcb4e801014445327b2a36e04f2bebf06118
HasalKenula <hasalkenula255@gmail.com>
2026-02-12 23:16:04 +0530

    added create slip to slipServiceImpl

--- src/main/java/com/worksure/worksure/service/SlipServiceImpl.java
+++ src/main/java/com/worksure/worksure/service/SlipServiceImpl.java
@@ -1,11 +1,19 @@
 package com.worksure.worksure.service;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.worksure.worksure.entity.Slip;
+import com.worksure.worksure.repository.SlipRepository;
 
 @Service
-public class SlipServiceImpl implements SlipService{
-   
+public class SlipServiceImpl implements SlipService {
+
+    @Autowired
+    private SlipRepository slipRepository;
+
+    @Override
+    public Slip createSlip(Slip slip) {
+        return slipRepository.save(slip);
+    }
 }
